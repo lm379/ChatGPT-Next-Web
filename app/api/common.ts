69867082e87e69a60bd8bb4bd3c23fbf341231db
lm379 <69002314+lm379@users.noreply.github.com>
2023-08-15 16:14:16 +0800

    Update common.ts

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const OPENAI_URL = "openkey.cloud";
+export const OPENAI_URL = "api.openai.com";
 const DEFAULT_PROTOCOL = "https";
 const PROTOCOL = process.env.PROTOCOL || DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL || OPENAI_URL;
